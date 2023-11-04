2222d60a95b47208d0b4a7d58ef4370bca67f4d7
mizok <mizok.huang@neutec.com.tw>
2023-11-04 22:27:40 +0800

    fix: 修正commitlint 相關 -p3

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -3,38 +3,24 @@ import { UserConfig } from '@commitlint/types'
 const config: UserConfig = {
   // 定義你的 Commitlint 規則
   rules: {
-    typeEnum: [
+    'type-enum': [
       2,
       'always',
       ['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'perf', 'test'],
     ],
-    scopeEmpty: [
-      2,
-      'always',
-      [
-        'breaking-change',
-        'feat',
-        'fix',
-        'refactor',
-        'chore',
-        'docs',
-        'style',
-        'test',
-        'perf',
-      ],
-    ],
-    scopeEnum: [
+    'scope-empty': [2, 'always'],
+    'scope-enum': [
       2,
       'always',
       ['core', 'ui', 'config', 'build', 'docs', 'chore', 'test'],
     ],
-    subjectMaxLength: [2, 'always', 100],
-    headerMaxLength: [2, 'always', 100],
+    'subject-max-length': [2, 'always', 100],
+    'header-max-length': [2, 'always', 100],
   },
-  parserPreset: {
+  'parser-preset': {
     // 自定義 commit 訊息解析器（如果需要的話）
-    parserOpts: {
-      issuePrefixes: ['#'], // 定義 issue 號碼的前綴符號
+    'parser-opts': {
+      'issue-prefixes': ['#'], // 定義 issue 號碼的前綴符號
     },
   },
 }
