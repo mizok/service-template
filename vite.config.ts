55930976845ff2bf98f755fa0ee4b45e2f20dd28
mizok <mizok.huang@neutec.com.tw>
2023-11-04 23:44:14 +0800

    fix:ESModule 相關

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ const config: UserConfig = {
   build: {
     outDir: 'dist', // 打包後的文件輸出目錄
     target: 'node16.17', // Node.js 的目標版本
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
     rollupOptions: {
       input: {
         main: 'src/main.ts', // 指定入口文件，這裡的路徑根據你的專案實際情況設置
